15bc13f6cef63252de93a94a748da97d6699eb6b
Jhoni Rincon <jhoni.789.-@hotmail.com>
2020-05-07 18:52:37 -0500

    Se configuraron las rutas del proyecto soloestan de prueva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { TopSdenavComponent } from './top-sdenav/top-sdenav.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'map', component: MapComponent},
+  {path: 'sdenav', component: TopSdenavComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login/login.component';
 import { TopSdenavComponent } from './top-sdenav/top-sdenav.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TopSdenavComponent
+    TopSdenavComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
